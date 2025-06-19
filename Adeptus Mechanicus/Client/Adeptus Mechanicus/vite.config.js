94c19961992cdd5f8367011f618ba656935c5083
NotFreakgen <notfreakgen@gmail.com>
2025-06-19 15:36:11 +0300

    Ready v2

--- Adeptus Mechanicus/Client/Adeptus Mechanicus/vite.config.js
+++ Adeptus Mechanicus/Client/Adeptus Mechanicus/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api' : {
-        target: 'http://172.18.0.4:8080',
+        target: 'http://172.18.0.3:8080',
         changeOrigin: true,
         secure: false,
       }
